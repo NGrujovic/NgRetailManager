0472de8d1fd02bf9eef27cbdcc994f30d29337b6
NGrujovic <ngrujovic99@gmail.com>
2023-01-16 14:02:16 +0100

    Add Swagger Docs
    Adds Swashbuckle/Swagger to Api
    for documentation and testing.

---
+++ NRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace NRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            //Adding parameter to every operation, basicly adding filter to see if user is logged in(if he has a token) and if he is 
+            // authorized to use operation
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name="Authorization",
+                @in = "header",
+                description = "acess token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
