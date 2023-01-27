4e8af484c93347b555710564f797e86bf54437cf
NGrujovic <ngrujovic99@gmail.com>
2023-01-27 19:21:24 +0100

    Wire Up login form to API
    Connects login button to the Auth. Api endpoint
    (/token). Gets back bearer token or an exception if failed

---
+++ NgRMDesktopUserInterface/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using NgRMDesktopUserInterface.Models;
+using System.Threading.Tasks;
+
+namespace NgRMDesktopUserInterface.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
